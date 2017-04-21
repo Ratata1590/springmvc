b8ef6903e226dcfba546a1d6866d951c20596fc6
EHD1HC <EHD1HC@HCUT2710C.hc.apac.bosch.com>
2017-04-21 13:02:10 +0700

    clean up

--- src/main/java/com/ratata/controller/NativeQueryEndpointController.java
+++ src/main/java/com/ratata/controller/NativeQueryEndpointController.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
