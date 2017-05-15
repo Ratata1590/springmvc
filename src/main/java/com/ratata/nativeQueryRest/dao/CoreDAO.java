42d0ef3ac40a0e56fed5890254392573134fc6c8
EHD1HC <EHD1HC@HCUT2710C.hc.apac.bosch.com>
2017-05-15 18:28:28 +0700

    update linkObject, cleanup code

--- src/main/java/com/ratata/nativeQueryRest/dao/CoreDAO.java
+++ src/main/java/com/ratata/nativeQueryRest/dao/CoreDAO.java
@@ -1,20 +1,14 @@
 package com.ratata.nativeQueryRest.dao;
 
-import java.util.Map;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.ratata.nativeQueryRest.pojo.NativeQueryParam;
 
 public interface CoreDAO {
-	public Object nativeQuery(NativeQueryParam nativeQueryParam) throws Exception;
+  public Object nativeQuery(NativeQueryParam nativeQueryParam) throws Exception;
 
-	public Object processQueryObject(JsonNode queryObject, JsonNode param) throws Exception;
+  public Object processQueryObject(JsonNode queryObject, JsonNode param);
 
-	public Object processSingleNestedNode(Map<String, Object> rootResult, JsonNode insideObject) throws Exception;
+  public void saveObject(JsonNode obj, String className) throws Exception;
 
-	public void saveObject(JsonNode obj, String className) throws Exception;
-
-	public Object saveLinkedObject(JsonNode obj) throws Exception;
-	
-	public Object saveunLinkedObject(JsonNode obj) throws Exception;
+  public Object linkObject(JsonNode obj);
 }
--- src/main/java/com/ratata/nativeQueryRest/dao/DynamicDTODAO.java
+++ src/main/java/com/ratata/nativeQueryRest/dao/DynamicDTODAO.java
@@ -6,12 +6,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public interface DynamicDTODAO {
-	public Object nativeWithDynamicPojo(JsonNode pojo) throws Exception;
+  public Object nativeWithDynamicPojo(JsonNode pojo);
 
-	public Object nativeWithDynamicPojo(JsonNode node, JsonNode param) throws Exception;
+  public Object nativeWithDynamicPojo(JsonNode node, JsonNode param);
 
-	public List<Object> transationNativeQuery(String query) throws Exception;
+  public List<Object> transationNativeQuery(String query) throws Exception;
 
-	public List<Object> transationNativeQuery(ArrayNode query) throws Exception;
+  public List<Object> transationNativeQuery(ArrayNode query);
 
 }
