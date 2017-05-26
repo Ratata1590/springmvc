7b83e95d83fad444686310868b3d194b5a4f5879
EHD1HC <EHD1HC@HCUT2710C.hc.apac.bosch.com>
2017-05-26 17:20:26 +0700

    update actionObject

---
+++ src/main/java/com/ratata/ObjectEndpoint/pojo/LinkObject.java
@@ -0,0 +1,13 @@
+package com.ratata.ObjectEndpoint.pojo;
+
+public class LinkObject {
+  private Integer objectId;
+
+  public Integer getObjectId() {
+    return objectId;
+  }
+
+  public void setObjectId(Integer objectId) {
+    this.objectId = objectId;
+  }
+}
