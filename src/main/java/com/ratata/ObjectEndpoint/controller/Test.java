50ea1cde781de51f0a6758252759762d2f4716df
Ratata1590 <Ratata1590@Ratata1590-PC>
2017-05-27 08:03:08 +0700

    update example

--- src/main/java/com/ratata/ObjectEndpoint/controller/Test.java
+++ src/main/java/com/ratata/ObjectEndpoint/controller/Test.java
@@ -1,33 +1,31 @@
 package com.ratata.ObjectEndpoint.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Test {
 	public ClassLoader classLoader;
-	public final String ObjectMapper = "com.fasterxml.jackson.databind.ObjectMapper";
-	public final String JsonNode = "com.fasterxml.jackson.databind.JsonNode";
-	public final String ArrayNode = "com.fasterxml.jackson.databind.node.ArrayNode";
-	public final String Mapper = "com.ratata.nativeQueryRest.utils.Mapper";
-	public final String[] classNameList = { ObjectMapper, JsonNode, ArrayNode, Mapper };
-	public Map<String, Class<?>> classList = new HashMap<String, Class<?>>();
+	public final int className = 0;
+	public final int getClass = 1;
+	public final Object[] ObjectMapper = { "com.fasterxml.jackson.databind.ObjectMapper", null };
+	public final Object[] JsonNode = { "com.fasterxml.jackson.databind.JsonNode", null };
+	public final Object[] ArrayNode = { "com.fasterxml.jackson.databind.node.ArrayNode", null };
+	public final Object[] Mapper = { "com.ratata.nativeQueryRest.utils.Mapper", null };
+	public final Object[][] classNameList = { ObjectMapper, JsonNode, ArrayNode, Mapper };
 
 	public void loadAll() throws Exception {
-		for (String str : classNameList) {
-			classList.put(str, classLoader.loadClass(str));
+		for (Object[] obj : classNameList) {
+			obj[getClass] = classLoader.loadClass((String) obj[className]);
 		}
 	}
 
-	public Object newClass(String className, Object... param) throws Exception {
+	public Object newClass(Object[] theClass, Object... param) throws Exception {
 		Class<?>[] classTypeList = new Class<?>[param.length];
 		for (int i = 0; i < param.length; i++) {
 			classTypeList[i] = param.getClass();
 		}
-		return classList.get(className).getConstructor(classTypeList).newInstance(param);
+		return ((Class<?>) theClass[getClass]).getConstructor(classTypeList).newInstance(param);
 	}
 
-	public Object getStaticProp(String className, String propName) throws Exception {
-		return classList.get(className).getDeclaredField(propName).get(null);
+	public Object getStaticProp(Object[] theClass, String propName) throws Exception {
+		return ((Class<?>) theClass[getClass]).getDeclaredField(propName).get(null);
 	}
 
 	public Test(ClassLoader classLoader) throws Exception {
