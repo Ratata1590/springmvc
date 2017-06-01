3da8776f591d06050d563a03ff6a3c4c452da78e
EHD1HC <EHD1HC@HCUT2710C.hc.apac.bosch.com>
2017-06-01 14:27:46 +0700

    add download to object (InputStream)

--- src/main/java/com/ratata/dynamicCodeRest/controller/DynamicCodeRestEndpointController.java
+++ src/main/java/com/ratata/dynamicCodeRest/controller/DynamicCodeRestEndpointController.java
@@ -1,12 +1,15 @@
 package com.ratata.dynamicCodeRest.controller;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
 import org.mdkt.compiler.InMemoryJavaCompiler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,109 +24,125 @@ import com.ratata.dynamicCodeRest.utils.ShareResourceFromSpringInterface;
 
 @RestController
 public class DynamicCodeRestEndpointController {
-	@Autowired
-	private ShareResourceFromSpringInterface shareResourceFromSpring;
+  @Autowired
+  private ShareResourceFromSpringInterface shareResourceFromSpring;
 
-	@PostConstruct
-	private void initResource() {
-		shareResourceFromSpring.loadAllSharedObj();
-	}
+  @PostConstruct
+  private void initResource() {
+    shareResourceFromSpring.loadAllSharedObj();
+  }
 
-	public static Map<String, String> sourceCodeList = new ConcurrentHashMap<String, String>();
+  public static Map<String, String> sourceCodeList = new ConcurrentHashMap<String, String>();
 
-	public static Map<String, Class<?>> classList = new ConcurrentHashMap<String, Class<?>>();
+  public static Map<String, Class<?>> classList = new ConcurrentHashMap<String, Class<?>>();
 
-	public static Map<String, DynamicObject> objList = new ConcurrentHashMap<String, DynamicObject>();
+  public static Map<String, DynamicObject> objList = new ConcurrentHashMap<String, DynamicObject>();
 
-	@RequestMapping(value = "/newClass", method = RequestMethod.POST)
-	public void newClass(@RequestBody String classbody, @RequestHeader String className,
-			@RequestHeader(required = false, defaultValue = "true") Boolean saveSource) throws Exception {
-		Class<?> theClass = InMemoryJavaCompiler.compile(className, classbody);
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		DynamicCodeUtil.loadAllClass(theClass, classLoader);
-		if (classList.containsKey(className)) {
-			removeClass(className);
-		}
-		classList.put(className, theClass);
-		if (saveSource) {
-			sourceCodeList.put(className, classbody);
-		}
-		System.gc();
-	}
+  @RequestMapping(value = "/newClass", method = RequestMethod.POST)
+  public void newClass(@RequestBody String classbody, @RequestHeader String className,
+      @RequestHeader(required = false, defaultValue = "true") Boolean saveSource) throws Exception {
+    Class<?> theClass = InMemoryJavaCompiler.compile(className, classbody);
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    DynamicCodeUtil.loadAllClass(theClass, classLoader);
+    if (classList.containsKey(className)) {
+      removeClass(className);
+    }
+    classList.put(className, theClass);
+    if (saveSource) {
+      sourceCodeList.put(className, classbody);
+    }
+    System.gc();
+  }
 
-	@RequestMapping(value = "/removeClass", method = RequestMethod.GET)
-	public void removeClass(@RequestHeader String className) throws Exception {
-		classList.remove(className);
-		sourceCodeList.remove(className);
-		for (String obj : objList.keySet()) {
-			if (obj.startsWith(className.concat(DynamicObject.SEPARATOR))) {
-				objList.remove(obj);
-			}
-			if (obj.startsWith(className.concat(DynamicObject.CLASS_SEPARATOR))) {
-				objList.remove(obj);
-			}
-		}
-		System.gc();
-	}
+  @RequestMapping(value = "/removeClass", method = RequestMethod.GET)
+  public void removeClass(@RequestHeader String className) throws Exception {
+    classList.remove(className);
+    sourceCodeList.remove(className);
+    for (String obj : objList.keySet()) {
+      if (obj.startsWith(className.concat(DynamicObject.SEPARATOR))) {
+        objList.remove(obj);
+      }
+      if (obj.startsWith(className.concat(DynamicObject.CLASS_SEPARATOR))) {
+        objList.remove(obj);
+      }
+    }
+    System.gc();
+  }
 
-	@RequestMapping(value = "/callClassMethod", method = RequestMethod.POST)
-	public Object callClassMethod(@RequestHeader(required = true) String className,
-			@RequestHeader(required = true) String methodName,
-			@RequestHeader(required = false, defaultValue = "false") Boolean save, @RequestBody Object... param)
-			throws Exception {
-		DynamicObject result = DynamicObject.callClassMethod(classList.get(className), methodName, param);
-		if (save && result != null) {
-			objList.put(result.getObjName(), result);
-		}
-		return result;
-	}
+  @RequestMapping(value = "/callClassMethod", method = RequestMethod.POST)
+  public Object callClassMethod(@RequestHeader(required = true) String className,
+      @RequestHeader(required = true) String methodName,
+      @RequestHeader(required = false, defaultValue = "false") Boolean save,
+      @RequestHeader(required = false, defaultValue = "false") Boolean download,
+      HttpServletResponse response, @RequestBody Object... param) throws Exception {
+    DynamicObject result =
+        DynamicObject.callClassMethod(classList.get(className), methodName, param);
+    if (save && result != null) {
+      objList.put(result.getObjName(), result);
+    }
+    if (download) {
+      response.setContentType("application/x-msdownload");
+      response.setHeader("Content-disposition", "attachment; filename=result");
+      IOUtils.copy((InputStream) result.getObj(), response.getOutputStream());
+      return null;
+    }
+    return result;
+  }
 
-	@RequestMapping(value = "/classList", method = RequestMethod.GET)
-	public Object classList(@RequestHeader(required = false, defaultValue = "false") Boolean showSource)
-			throws Exception {
-		ArrayList<Object> result = new ArrayList<Object>();
-		if (showSource) {
-			result.add(sourceCodeList);
-		}
-		String[] clazzList = classList.keySet().toArray(new String[classList.keySet().size()]);
-		Arrays.sort(clazzList);
-		result.add(clazzList);
-		return result;
-	}
+  @RequestMapping(value = "/classList", method = RequestMethod.GET)
+  public Object classList(
+      @RequestHeader(required = false, defaultValue = "false") Boolean showSource)
+      throws Exception {
+    ArrayList<Object> result = new ArrayList<Object>();
+    if (showSource) {
+      result.add(sourceCodeList);
+    }
+    String[] clazzList = classList.keySet().toArray(new String[classList.keySet().size()]);
+    Arrays.sort(clazzList);
+    result.add(clazzList);
+    return result;
+  }
 
-	@RequestMapping(value = "/newObj", method = RequestMethod.GET)
-	public String newObj(@RequestHeader String className) throws Exception {
-		Object ob = classList.get(className).getConstructor().newInstance();
-		String instanceId = className + ":" + ob.hashCode();
-		objList.put(instanceId, new DynamicObject(ob, instanceId));
-		System.gc();
-		return instanceId;
-	}
+  @RequestMapping(value = "/newObj", method = RequestMethod.GET)
+  public String newObj(@RequestHeader String className) throws Exception {
+    Object ob = classList.get(className).getConstructor().newInstance();
+    String instanceId = className + ":" + ob.hashCode();
+    objList.put(instanceId, new DynamicObject(ob, instanceId));
+    System.gc();
+    return instanceId;
+  }
 
-	@RequestMapping(value = "/removeObj", method = RequestMethod.GET)
-	public void removeObj(@RequestHeader(required = true) String instanceId) throws Exception {
-		objList.remove(instanceId);
-		System.gc();
-	}
+  @RequestMapping(value = "/removeObj", method = RequestMethod.GET)
+  public void removeObj(@RequestHeader(required = true) String instanceId) throws Exception {
+    objList.remove(instanceId);
+    System.gc();
+  }
 
-	@RequestMapping(value = "/objList", method = RequestMethod.GET)
-	public Object objList() throws Exception {
-		String[] result = objList.keySet().toArray(new String[objList.keySet().size()]);
-		Arrays.sort(result);
-		return result;
-	}
+  @RequestMapping(value = "/objList", method = RequestMethod.GET)
+  public Object objList() throws Exception {
+    String[] result = objList.keySet().toArray(new String[objList.keySet().size()]);
+    Arrays.sort(result);
+    return result;
+  }
 
-	@RequestMapping(value = "/callObjMethod", method = RequestMethod.POST)
-	public Object callObjMethod(@RequestHeader(required = true) String instanceId,
-			@RequestHeader(required = true) String methodName,
-			@RequestHeader(required = false, defaultValue = "false") Boolean save, @RequestBody Object... param)
-			throws Exception {
-		DynamicObject obj = objList.get(instanceId);
-		DynamicObject result = obj.callObjMethod(methodName, param);
-		if (save && result != null) {
-			objList.put(result.getObjName(), result);
-		}
-		return result;
-	}
+  @RequestMapping(value = "/callObjMethod", method = RequestMethod.POST)
+  public Object callObjMethod(@RequestHeader(required = true) String instanceId,
+      @RequestHeader(required = true) String methodName,
+      @RequestHeader(required = false, defaultValue = "false") Boolean save,
+      @RequestHeader(required = false, defaultValue = "false") Boolean download,
+      HttpServletResponse response, @RequestBody Object... param) throws Exception {
+    DynamicObject obj = objList.get(instanceId);
+    DynamicObject result = obj.callObjMethod(methodName, param);
+    if (save && result != null) {
+      objList.put(result.getObjName(), result);
+    }
+    if (download) {
+      response.setContentType("application/x-msdownload");
+      response.setHeader("Content-disposition", "attachment; filename=result");
+      IOUtils.copy((InputStream) result.getObj(), response.getOutputStream());
+      return null;
+    }
+    return result;
+  }
 
 }
--- src/main/java/com/ratata/dynamicCodeRest/controller/GenToolDynamicCode.java
+++ src/main/java/com/ratata/dynamicCodeRest/controller/GenToolDynamicCode.java
@@ -20,85 +20,87 @@ import com.ratata.dynamicCodeRest.utils.ThreadUtils;
 
 @RestController
 public class GenToolDynamicCode {
-	public static final String importList = "importList";
-	public static final String autowireList = "autowireList";
-	public static final String importString = "import ";
-	public static final ObjectMapper mapper = new ObjectMapper();
+  public static final String importList = "importList";
+  public static final String autowireList = "autowireList";
+  public static final String importString = "import ";
+  public static final ObjectMapper mapper = new ObjectMapper();
 
-	@RequestMapping(value = "/checkInputType", method = RequestMethod.POST)
-	public Object checkInputType(@RequestBody Object data) {
-		return new ObjectContainer(data);
-	}
+  @RequestMapping(value = "/checkInputType", method = RequestMethod.POST)
+  public Object checkInputType(@RequestBody Object data) {
+    return new ObjectContainer(data);
+  }
 
-	@RequestMapping(value = "/genDynamicCodeTemplate", method = RequestMethod.POST)
-	public String genDynamicCodeTemplate(@RequestBody JsonNode data) {
-		StringBuilder result = new StringBuilder();
-		if (data.get(importList) != null) {
-			String impList = data.get(importList).textValue();
-			for (String imp : impList.split(";")) {
-				String classNameFull = imp.substring(importString.length(), imp.length());
-				String classNameShort = new String(classNameFull).substring(classNameFull.lastIndexOf(".") + 1);
-				result.append(
-						String.format("public static Object $C_%1$s = \"%2$s\";\n", classNameShort, classNameFull));
-			}
-			result.append("\n");
-		}
-		if (data.get(autowireList) != null) {
-			String auList = data.get(autowireList).textValue();
-			for (String imp : auList.split(";")) {
-				String classNameFull = imp.substring(importString.length(), imp.length());
-				String classNameShort = new String(classNameFull).substring(classNameFull.lastIndexOf(".") + 1);
-				result.append(
-						String.format("public static Object $Sp_%1$s = \"%2$s\";\n", classNameShort, classNameFull));
-			}
-		}
-		return result.toString();
-	}
+  @RequestMapping(value = "/genDynamicCodeTemplate", method = RequestMethod.POST)
+  public String genDynamicCodeTemplate(@RequestBody JsonNode data) {
+    StringBuilder result = new StringBuilder();
+    if (data.get(importList) != null) {
+      String impList = data.get(importList).textValue();
+      for (String imp : impList.split(";")) {
+        String classNameFull = imp.substring(importString.length(), imp.length());
+        String classNameShort =
+            new String(classNameFull).substring(classNameFull.lastIndexOf(".") + 1);
+        result.append(String.format("public static Object $C_%1$s = \"%2$s\";\n", classNameShort,
+            classNameFull));
+      }
+      result.append("\n");
+    }
+    if (data.get(autowireList) != null) {
+      String auList = data.get(autowireList).textValue();
+      for (String imp : auList.split(";")) {
+        String classNameFull = imp.substring(importString.length(), imp.length());
+        String classNameShort =
+            new String(classNameFull).substring(classNameFull.lastIndexOf(".") + 1);
+        result.append(String.format("public static Object $Sp_%1$s = \"%2$s\";\n", classNameShort,
+            classNameFull));
+      }
+    }
+    return result.toString();
+  }
 
-	private final XmlMapper xmlMapper = new XmlMapper();
+  private final XmlMapper xmlMapper = new XmlMapper();
 
-	@RequestMapping(value = "/convertMavenToJson", method = RequestMethod.POST)
-	public Object convertMavenToJson(@RequestBody String mavenDependencies) throws Exception {
-		return xmlMapper.readTree(mavenDependencies);
-	}
+  @RequestMapping(value = "/convertMavenToJson", method = RequestMethod.POST)
+  public Object convertMavenToJson(@RequestBody String mavenDependencies) throws Exception {
+    return xmlMapper.readTree(mavenDependencies);
+  }
 
-	/**
-	 * junit,junit,3.8.1,junit-3.8.1.jar
-	 * org,springframework,spring-webmvc,4.0.1.RELEASE,spring-webmvc-4.0.1.RELEASE.jar
-	 * org,springframework,spring-beans,4.0.1.RELEASE,spring-beans-4.0.1.RELEASE.jar
-	 * org,springframework,spring-context,4.0.1.RELEASE,spring-context-4.0.1.RELEASE.jar
-	 * org,springframework,spring-core,4.0.1.RELEASE,spring-core-4.0.1.RELEASE.jar
-	 **/
-	@RequestMapping(value = "/convertJarListToJson", method = RequestMethod.POST)
-	public Object convertJarListToJson(@RequestBody String jarList) throws Exception {
-		ArrayNode result = mapper.createArrayNode();
-		String[] lines = jarList.split("\r\n");
-		for (String line : lines) {
-			ObjectNode item = mapper.createObjectNode();
-			String[] lineItems = line.split(",");
-			List<String> groupId = Arrays.asList(Arrays.copyOfRange(lineItems, 0, lineItems.length - 3));
-			item.put(MavenRepoEndpointController.keyGroupId, String.join(".", groupId));
-			item.put(MavenRepoEndpointController.keyArtifactId, lineItems[lineItems.length - 3]);
-			item.put(MavenRepoEndpointController.keyVersion, lineItems[lineItems.length - 2]);
-			result.add(item);
-		}
-		return result;
-	}
+  /**
+   * junit,junit,3.8.1,junit-3.8.1.jar
+   * org,springframework,spring-webmvc,4.0.1.RELEASE,spring-webmvc-4.0.1.RELEASE.jar
+   * org,springframework,spring-beans,4.0.1.RELEASE,spring-beans-4.0.1.RELEASE.jar
+   * org,springframework,spring-context,4.0.1.RELEASE,spring-context-4.0.1.RELEASE.jar
+   * org,springframework,spring-core,4.0.1.RELEASE,spring-core-4.0.1.RELEASE.jar
+   **/
+  @RequestMapping(value = "/convertJarListToJson", method = RequestMethod.POST)
+  public Object convertJarListToJson(@RequestBody String jarList) throws Exception {
+    ArrayNode result = mapper.createArrayNode();
+    String[] lines = jarList.split("\r\n");
+    for (String line : lines) {
+      ObjectNode item = mapper.createObjectNode();
+      String[] lineItems = line.split(",");
+      List<String> groupId = Arrays.asList(Arrays.copyOfRange(lineItems, 0, lineItems.length - 3));
+      item.put(MavenRepoEndpointController.keyGroupId, String.join(".", groupId));
+      item.put(MavenRepoEndpointController.keyArtifactId, lineItems[lineItems.length - 3]);
+      item.put(MavenRepoEndpointController.keyVersion, lineItems[lineItems.length - 2]);
+      result.add(item);
+    }
+    return result;
+  }
 
-	@RequestMapping(value = "/getAllThead", method = RequestMethod.GET)
-	public Object getAllThead() throws Exception {
-		Map<String, Object> result = new HashMap<String, Object>();
-		result.put("getAllDaemonThreads", ThreadUtils.getAllThreadInfos());
-		return result;
-	}
+  @RequestMapping(value = "/getAllThead", method = RequestMethod.GET)
+  public Object getAllThead() throws Exception {
+    Map<String, Object> result = new HashMap<String, Object>();
+    result.put("getAllDaemonThreads", ThreadUtils.getAllThreadInfos());
+    return result;
+  }
 
-	@RequestMapping(value = "/freeMem", method = RequestMethod.GET)
-	public Object freeMem() throws Exception {
-		ObjectNode node = mapper.createObjectNode();
-		node.put("total", String.valueOf(Runtime.getRuntime().totalMemory()) + "/"
-				+ String.valueOf(Runtime.getRuntime().maxMemory()));
-		node.put("freed", String.valueOf(Runtime.getRuntime().freeMemory()));
-		node.put("processor", Runtime.getRuntime().availableProcessors());
-		return node;
-	}
+  @RequestMapping(value = "/freeMem", method = RequestMethod.GET)
+  public Object freeMem() throws Exception {
+    ObjectNode node = mapper.createObjectNode();
+    node.put("total", String.valueOf(Runtime.getRuntime().totalMemory()) + "/"
+        + String.valueOf(Runtime.getRuntime().maxMemory()));
+    node.put("freed", String.valueOf(Runtime.getRuntime().freeMemory()));
+    node.put("processor", Runtime.getRuntime().availableProcessors());
+    return node;
+  }
 }
