ffbbf459ec305bb418f4302754d5dfcf49f737c4
Ratata1590 <Ratata1590@Ratata1590-PC>
2017-06-03 22:00:01 +0700

    format source

--- src/main/java/com/ratata/dynamicCodeRest/controller/GenToolDynamicCode.java
+++ src/main/java/com/ratata/dynamicCodeRest/controller/GenToolDynamicCode.java
@@ -16,57 +16,55 @@ import com.ratata.dynamicCodeRest.utils.ThreadUtils;
 
 @RestController
 public class GenToolDynamicCode {
-  public static final String importList = "importList";
-  public static final String autowireList = "autowireList";
-  public static final String importString = "import ";
-  public static final ObjectMapper mapper = new ObjectMapper();
+	public static final String importList = "importList";
+	public static final String autowireList = "autowireList";
+	public static final String importString = "import ";
+	public static final ObjectMapper mapper = new ObjectMapper();
 
-  @RequestMapping(value = "/checkInputType", method = RequestMethod.POST)
-  public Object checkInputType(@RequestBody Object data) {
-    return new ObjectContainer(data);
-  }
+	@RequestMapping(value = "/checkInputType", method = RequestMethod.POST)
+	public Object checkInputType(@RequestBody Object data) {
+		return new ObjectContainer(data);
+	}
 
-  @RequestMapping(value = "/genDynamicCodeTemplate", method = RequestMethod.POST)
-  public String genDynamicCodeTemplate(@RequestBody JsonNode data) {
-    StringBuilder result = new StringBuilder();
-    if (data.get(importList) != null) {
-      String impList = data.get(importList).textValue();
-      for (String imp : impList.split(";")) {
-        String classNameFull = imp.substring(importString.length(), imp.length());
-        String classNameShort =
-            new String(classNameFull).substring(classNameFull.lastIndexOf(".") + 1);
-        result.append(String.format("public static Object $C_%1$s = \"%2$s\";\n", classNameShort,
-            classNameFull));
-      }
-      result.append("\n");
-    }
-    if (data.get(autowireList) != null) {
-      String auList = data.get(autowireList).textValue();
-      for (String imp : auList.split(";")) {
-        String classNameFull = imp.substring(importString.length(), imp.length());
-        String classNameShort =
-            new String(classNameFull).substring(classNameFull.lastIndexOf(".") + 1);
-        result.append(String.format("public static Object $Sp_%1$s = \"%2$s\";\n", classNameShort,
-            classNameFull));
-      }
-    }
-    return result.toString();
-  }
+	@RequestMapping(value = "/genDynamicCodeTemplate", method = RequestMethod.POST)
+	public String genDynamicCodeTemplate(@RequestBody JsonNode data) {
+		StringBuilder result = new StringBuilder();
+		if (data.get(importList) != null) {
+			String impList = data.get(importList).textValue();
+			for (String imp : impList.split(";")) {
+				String classNameFull = imp.substring(importString.length(), imp.length());
+				String classNameShort = new String(classNameFull).substring(classNameFull.lastIndexOf(".") + 1);
+				result.append(
+						String.format("public static Object $C_%1$s = \"%2$s\";\n", classNameShort, classNameFull));
+			}
+			result.append("\n");
+		}
+		if (data.get(autowireList) != null) {
+			String auList = data.get(autowireList).textValue();
+			for (String imp : auList.split(";")) {
+				String classNameFull = imp.substring(importString.length(), imp.length());
+				String classNameShort = new String(classNameFull).substring(classNameFull.lastIndexOf(".") + 1);
+				result.append(
+						String.format("public static Object $Sp_%1$s = \"%2$s\";\n", classNameShort, classNameFull));
+			}
+		}
+		return result.toString();
+	}
 
-  @RequestMapping(value = "/getAllThead", method = RequestMethod.GET)
-  public Object getAllThead() throws Exception {
-    Map<String, Object> result = new HashMap<String, Object>();
-    result.put("getAllDaemonThreads", ThreadUtils.getAllThreadInfos());
-    return result;
-  }
+	@RequestMapping(value = "/getAllThead", method = RequestMethod.GET)
+	public Object getAllThead() throws Exception {
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("getAllDaemonThreads", ThreadUtils.getAllThreadInfos());
+		return result;
+	}
 
-  @RequestMapping(value = "/freeMem", method = RequestMethod.GET)
-  public Object freeMem() throws Exception {
-    ObjectNode node = mapper.createObjectNode();
-    node.put("total", String.valueOf(Runtime.getRuntime().totalMemory()) + "/"
-        + String.valueOf(Runtime.getRuntime().maxMemory()));
-    node.put("freed", String.valueOf(Runtime.getRuntime().freeMemory()));
-    node.put("processor", Runtime.getRuntime().availableProcessors());
-    return node;
-  }
+	@RequestMapping(value = "/freeMem", method = RequestMethod.GET)
+	public Object freeMem() throws Exception {
+		ObjectNode node = mapper.createObjectNode();
+		node.put("total", String.valueOf(Runtime.getRuntime().totalMemory()) + "/"
+				+ String.valueOf(Runtime.getRuntime().maxMemory()));
+		node.put("freed", String.valueOf(Runtime.getRuntime().freeMemory()));
+		node.put("processor", Runtime.getRuntime().availableProcessors());
+		return node;
+	}
 }
