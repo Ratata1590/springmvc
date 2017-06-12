a2ab1b4bcd79872fefab9085b916145bbcc3ab09
EHD1HC <EHD1HC@HCUT2710C.hc.apac.bosch.com>
2017-06-12 10:27:50 +0700

    split socketControl

--- src/main/java/com/ratata/dynamicCodeRest/controller/SocketRestEndpointController.java
+++ src/main/java/com/ratata/dynamicCodeRest/controller/SocketRestEndpointController.java
@@ -19,117 +19,115 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SocketRestEndpointController {
 
-	private static Map<String, Socket> sessionList = new ConcurrentHashMap<String, Socket>();
-	private static Map<String, String> configList = new ConcurrentHashMap<String, String>();
+  private static Map<String, Socket> sessionList = new ConcurrentHashMap<String, Socket>();
+  private static Map<String, String> configList = new ConcurrentHashMap<String, String>();
 
-	@RequestMapping(value = "/socketHandler", method = RequestMethod.POST)
-	public static void socketHandler(@RequestBody byte[] data, @RequestHeader(required = true) String sockRestId)
-			throws Exception {
-		Socket socket = sessionList.get(sockRestId);
-		if (data.length != 0) {
-			try {
-				OutputStream out = socket.getOutputStream();
-				out.write(data);
-				out.flush();
-			} catch (Exception e) {
-				try {
-					socket.close();
-				} catch (Exception e2) {
-				}
-				sessionList.remove(sockRestId);
-				throw new Exception("socket closed");
-			}
-		}
-	}
+  @RequestMapping(value = "/socketHandler", method = RequestMethod.POST)
+  public static void socketHandler(@RequestBody byte[] data,
+      @RequestHeader(required = true) String sockRestId) throws Exception {
+    Socket socket = sessionList.get(sockRestId);
+    if (data.length != 0) {
+      try {
+        OutputStream out = socket.getOutputStream();
+        out.write(data);
+        out.flush();
+      } catch (Exception e) {
+        try {
+          socket.close();
+        } catch (Exception e2) {
+        }
+        sessionList.remove(sockRestId);
+        throw new Exception("socket closed");
+      }
+    }
+  }
 
-	@RequestMapping(value = "/socketHandler", method = RequestMethod.GET)
-	public static Object socketHandler(@RequestHeader(required = true) String sockRestId) throws Exception {
-		Socket socket = sessionList.get(sockRestId);
-		byte[] resultBuff;
-		byte[] tmpbuff = null;
-		try {
-			resultBuff = new byte[socket.getReceiveBufferSize()];
-			tmpbuff = new byte[socket.getInputStream().read(resultBuff, 0, resultBuff.length)];
-			System.arraycopy(resultBuff, 0, tmpbuff, 0, tmpbuff.length);
-		} catch (Exception e) {
-			try {
-				socket.close();
-			} catch (Exception e2) {
-			}
-			sessionList.remove(sockRestId);
-			throw new Exception("socket closed");
-		}
-		return tmpbuff;
-	}
+  @RequestMapping(value = "/socketHandler", method = RequestMethod.GET)
+  public static Object socketHandler(@RequestHeader(required = true) String sockRestId)
+      throws Exception {
+    Socket socket = sessionList.get(sockRestId);
+    byte[] resultBuff;
+    byte[] tmpbuff = null;
+    try {
+      resultBuff = new byte[socket.getReceiveBufferSize()];
+      tmpbuff = new byte[socket.getInputStream().read(resultBuff, 0, resultBuff.length)];
+      System.arraycopy(resultBuff, 0, tmpbuff, 0, tmpbuff.length);
+    } catch (Exception e) {
+      try {
+        socket.close();
+      } catch (Exception e2) {
+      }
+      sessionList.remove(sockRestId);
+      throw new Exception("socket closed");
+    }
+    return tmpbuff;
+  }
 
-	@RequestMapping(value = "/socketControl", method = RequestMethod.POST)
-	public static String socketConnect(@RequestHeader(required = false) String sessionId,
-			@RequestHeader(required = true) String action, @RequestHeader(required = false) String host,
-			@RequestHeader(required = false) Integer port) throws Exception {
-		switch (action) {
-		case "connect":
-			String tmpsessionId = getSaltString();
-			while (sessionList.containsKey(tmpsessionId)) {
-				tmpsessionId = getSaltString();
-			}
-			Socket sock = openSocket(host, port);
-			sessionList.put(tmpsessionId, sock);
-			configList.put(tmpsessionId, host + ":" + port);
-			return tmpsessionId;
-		case "diconnect":
-			try {
-				sessionList.get(sessionId).close();
-			} catch (Exception e) {
-			}
-			sessionList.remove(sessionId);
-			configList.remove(sessionId);
-			return null;
-		}
-		return null;
-	}
+  @RequestMapping(value = "/socketControl/connect", method = RequestMethod.POST)
+  public static String socketControlConnect(@RequestHeader String host, @RequestHeader Integer port)
+      throws Exception {
+    String tmpsessionId = getSaltString();
+    while (sessionList.containsKey(tmpsessionId)) {
+      tmpsessionId = getSaltString();
+    }
+    Socket sock = openSocket(host, port);
+    sessionList.put(tmpsessionId, sock);
+    configList.put(tmpsessionId, host + ":" + port);
+    return tmpsessionId;
+  }
 
-	@RequestMapping(value = "/socketList", method = RequestMethod.GET)
-	public static Object socketList() {
-		return configList;
-	}
+  @RequestMapping(value = "/socketControl/disconnect", method = RequestMethod.POST)
+  public static void socketControlDisconnect(@RequestHeader String sessionId) throws Exception {
+    try {
+      sessionList.get(sessionId).close();
+    } catch (Exception e) {
+    }
+    sessionList.remove(sessionId);
+    configList.remove(sessionId);
+  }
 
-	@RequestMapping(value = "/socketClear", method = RequestMethod.GET)
-	public static void socketClear() {
-		for (String id : sessionList.keySet()) {
-			try {
-				sessionList.get(id).close();
-			} catch (Exception e) {
-			}
-			sessionList.remove(id);
-			configList.remove(id);
-		}
-	}
+  @RequestMapping(value = "/socketList", method = RequestMethod.GET)
+  public static Object socketList() {
+    return configList;
+  }
 
-	private static Socket openSocket(String server, int port) throws Exception {
-		Socket socket;
-		try {
-			InetAddress inteAddress = InetAddress.getByName(server);
-			SocketAddress socketAddress = new InetSocketAddress(inteAddress, port);
-			socket = new Socket();
-			int timeoutInMs = 10 * 1000;
-			socket.connect(socketAddress, timeoutInMs);
-			return socket;
-		} catch (SocketTimeoutException ste) {
-			ste.printStackTrace();
-			throw ste;
-		}
-	}
+  @RequestMapping(value = "/socketClear", method = RequestMethod.GET)
+  public static void socketClear() {
+    for (String id : sessionList.keySet()) {
+      try {
+        sessionList.get(id).close();
+      } catch (Exception e) {
+      }
+      sessionList.remove(id);
+      configList.remove(id);
+    }
+  }
 
-	public static String getSaltString() {
-		String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-		StringBuilder salt = new StringBuilder();
-		Random rnd = new Random();
-		while (salt.length() < 18) { // length of the random string.
-			int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-			salt.append(SALTCHARS.charAt(index));
-		}
-		String saltStr = salt.toString();
-		return saltStr;
+  private static Socket openSocket(String server, int port) throws Exception {
+    Socket socket;
+    try {
+      InetAddress inteAddress = InetAddress.getByName(server);
+      SocketAddress socketAddress = new InetSocketAddress(inteAddress, port);
+      socket = new Socket();
+      int timeoutInMs = 10 * 1000;
+      socket.connect(socketAddress, timeoutInMs);
+      return socket;
+    } catch (SocketTimeoutException ste) {
+      ste.printStackTrace();
+      throw ste;
+    }
+  }
 
-	}
+  public static String getSaltString() {
+    String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    StringBuilder salt = new StringBuilder();
+    Random rnd = new Random();
+    while (salt.length() < 18) { // length of the random string.
+      int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+      salt.append(SALTCHARS.charAt(index));
+    }
+    String saltStr = salt.toString();
+    return saltStr;
+
+  }
 }
