4222b1a7ba96aba2ebd7b83e7663bd8227d5696e
Ratata1590 <Ratata1590@Ratata1590-PC>
2017-06-13 00:02:22 +0700

    add method

--- src/main/java/com/ratata/dynamicCodeRest/controller/mirrorForce/BotPoolEndpointController.java
+++ src/main/java/com/ratata/dynamicCodeRest/controller/mirrorForce/BotPoolEndpointController.java
@@ -7,13 +7,14 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BotPoolEndpointController {
 	private static Map<String, LinkedList<String>> botConnectionPool = new ConcurrentHashMap<String, LinkedList<String>>();
 
-	@RequestMapping(value = "/botInit")
+	@RequestMapping(value = "/botInit", method = RequestMethod.POST)
 	public static List<String> botInit(@RequestHeader String botName, @RequestHeader int poolSize) {
 		LinkedList<String> thePool = new LinkedList<String>();
 		for (int i = 0; i < poolSize; i++) {
@@ -26,7 +27,7 @@ public class BotPoolEndpointController {
 		return thePool;
 	}
 
-	@RequestMapping(value = "/botCleanPool")
+	@RequestMapping(value = "/botCleanPool", method = RequestMethod.POST)
 	public static void botCleanPool(@RequestHeader String botName) {
 		if (botConnectionPool.containsKey(botName)) {
 			for (String conn : botConnectionPool.get(botName)) {
@@ -36,14 +37,14 @@ public class BotPoolEndpointController {
 		}
 	}
 
-	@RequestMapping(value = "/botAddConnection")
+	@RequestMapping(value = "/botAddConnection", method = RequestMethod.POST)
 	public static String botAddConnection(@RequestHeader String botName) {
 		String conn = MirrorForceEndpointController.mirrorControlCreate();
 		botConnectionPool.get(botName).push(conn);
 		return conn;
 	}
 
-	@RequestMapping(value = "/botGetConnection")
+	@RequestMapping(value = "/botGetConnection", method = RequestMethod.POST)
 	public static String botGetConnection(@RequestHeader String botName) throws Exception {
 		String conn = botConnectionPool.get(botName).pop();
 		String client = MirrorForceEndpointController.mirrorControlCreate();
