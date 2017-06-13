14933bfa66f5a525753cc79df5b295f7f8b01fd5
EHD1HC <EHD1HC@HCUT2710C.hc.apac.bosch.com>
2017-06-13 14:59:53 +0700

    update mirror force

--- src/main/java/com/ratata/dynamicCodeRest/controller/mirrorForce/BotPoolEndpointController.java
+++ src/main/java/com/ratata/dynamicCodeRest/controller/mirrorForce/BotPoolEndpointController.java
@@ -11,47 +11,57 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BotPoolEndpointController {
-	private static Map<String, LinkedList<String>> botConnectionPool = new ConcurrentHashMap<String, LinkedList<String>>();
+  private static Map<String, LinkedList<String>> botConnectionPool =
+      new ConcurrentHashMap<String, LinkedList<String>>();
 
-	// @RequestMapping(value = "/botInit", method = RequestMethod.POST)
-	// public static List<String> botInit(@RequestHeader String botName,
-	// @RequestHeader int poolSize) {
-	// LinkedList<String> thePool = new LinkedList<String>();
-	// for (int i = 0; i < poolSize; i++) {
-	// thePool.push(MirrorForceEndpointController.mirrorControlCreate());
-	// }
-	// if (botConnectionPool.containsKey(botName)) {
-	// botCleanPool(botName);
-	// }
-	// botConnectionPool.put(botName, thePool);
-	// return thePool;
-	// }
+  // @RequestMapping(value = "/botInit", method = RequestMethod.POST)
+  // public static List<String> botInit(@RequestHeader String botName,
+  // @RequestHeader int poolSize) {
+  // LinkedList<String> thePool = new LinkedList<String>();
+  // for (int i = 0; i < poolSize; i++) {
+  // thePool.push(MirrorForceEndpointController.mirrorControlCreate());
+  // }
+  // if (botConnectionPool.containsKey(botName)) {
+  // botCleanPool(botName);
+  // }
+  // botConnectionPool.put(botName, thePool);
+  // return thePool;
+  // }
 
-	@RequestMapping(value = "/botCleanPool", method = RequestMethod.POST)
-	public static void botCleanPool(@RequestHeader String botName) {
-		if (botConnectionPool.containsKey(botName)) {
-			for (String conn : botConnectionPool.get(botName)) {
-				botConnectionPool.remove(conn);
-				MirrorForceEndpointController.mirrorControlDisconnect(conn);
-			}
-		}
-	}
+  @RequestMapping(value = "/botInit", method = RequestMethod.POST)
+  public static void botInit(@RequestHeader String botName) {
+    if (!botConnectionPool.containsKey(botName)) {
+      botConnectionPool.put(botName, new LinkedList<String>());
+    }
+  }
 
-	@RequestMapping(value = "/botAddConnection", method = RequestMethod.POST)
-	public static String botAddConnection(@RequestHeader String botName) {
-		String conn = MirrorForceEndpointController.mirrorControlCreate();
-		if (!botConnectionPool.containsKey(botName)) {
-			botConnectionPool.put(botName, new LinkedList<String>());
-		}
-		botConnectionPool.get(botName).push(conn);
-		return conn;
-	}
+  @RequestMapping(value = "/botCleanPool", method = RequestMethod.POST)
+  public static void botCleanPool(@RequestHeader String botName) {
+    if (botConnectionPool.containsKey(botName)) {
+      for (String conn : botConnectionPool.get(botName)) {
+        MirrorForceEndpointController.mirrorControlDisconnect(conn);
+      }
+      botConnectionPool.get(botName).clear();
+    }
+  }
 
-	@RequestMapping(value = "/botGetConnection", method = RequestMethod.POST)
-	public static String botGetConnection(@RequestHeader String botName) throws Exception {
-		String conn = botConnectionPool.get(botName).pop();
-		String client = MirrorForceEndpointController.mirrorControlCreate();
-		MirrorForceEndpointController.mirrorControlConnectTwoWay(conn, client);
-		return client;
-	}
+  @RequestMapping(value = "/botList", method = RequestMethod.GET)
+  public static Object botList() {
+    return botConnectionPool;
+  }
+
+  @RequestMapping(value = "/botAddConnection", method = RequestMethod.POST)
+  public static String botAddConnection(@RequestHeader String botName) {
+    String conn = MirrorForceEndpointController.mirrorControlCreate();
+    botConnectionPool.get(botName).push(conn);
+    return conn;
+  }
+
+  @RequestMapping(value = "/botGetConnection", method = RequestMethod.POST)
+  public static String botGetConnection(@RequestHeader String botName) throws Exception {
+    String conn = botConnectionPool.get(botName).pop();
+    String client = MirrorForceEndpointController.mirrorControlCreate();
+    MirrorForceEndpointController.mirrorControlConnectTwoWay(conn, client);
+    return client;
+  }
 }
--- src/main/java/com/ratata/dynamicCodeRest/controller/mirrorForce/MirrorForceEndpointController.java
+++ src/main/java/com/ratata/dynamicCodeRest/controller/mirrorForce/MirrorForceEndpointController.java
@@ -19,148 +19,159 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 @RequestMapping("/mirror")
 @RestController
 public class MirrorForceEndpointController {
-	private static Map<String, PipedInputStream> inputStreamList = new ConcurrentHashMap<String, PipedInputStream>();
-	private static Map<String, PipedOutputStream> outputStreamList = new ConcurrentHashMap<String, PipedOutputStream>();
+  private static Map<String, PipedInputStream> inputStreamList =
+      new ConcurrentHashMap<String, PipedInputStream>();
+  private static Map<String, PipedOutputStream> outputStreamList =
+      new ConcurrentHashMap<String, PipedOutputStream>();
 
-	@RequestMapping(value = "/socketHandler", method = RequestMethod.POST)
-	public static void socketHandler(@RequestBody byte[] data, @RequestHeader(required = true) String sockRestId)
-			throws Exception {
-		PipedOutputStream out = outputStreamList.get(sockRestId);
-		if (data.length != 0) {
-			try {
-				out.write(data);
-				out.flush();
-			} catch (Exception e) {
-				try {
-					out.close();
-				} catch (Exception e2) {
-				}
-				outputStreamList.remove(sockRestId);
-				throw new Exception("socket closed");
-			}
-		}
-	}
+  @RequestMapping(value = "/socketHandler", method = RequestMethod.POST)
+  public static void socketHandler(@RequestBody byte[] data,
+      @RequestHeader(required = true) String sockRestId) throws Exception {
+    System.out.println("socketHandlerPost");
+    PipedOutputStream out = outputStreamList.get(sockRestId);
+    if (data.length != 0) {
+      try {
+        out.write(data);
+        out.flush();
+      } catch (Exception e) {
+        try {
+          out.close();
+        } catch (Exception e2) {
+        }
+        outputStreamList.remove(sockRestId);
+        throw new Exception("socket closed");
+      }
+    }
+    System.out.println("socketHandlerPostFinished");
+  }
 
-	@RequestMapping(value = "/socketHandler", method = RequestMethod.GET)
-	public static Object socketHandler(@RequestHeader(required = true) String sockRestId) throws Exception {
-		PipedInputStream in = inputStreamList.get(sockRestId);
-		int byteRead = in.available();
-		byte[] resultBuff = new byte[byteRead];
-		try {
-			in.read(resultBuff, 0, byteRead);
-		} catch (Exception e) {
-			try {
-				in.close();
-			} catch (Exception e2) {
-			}
-			inputStreamList.remove(sockRestId);
-			throw new Exception("socket closed");
-		}
-		return resultBuff;
-	}
+  @RequestMapping(value = "/socketHandler", method = RequestMethod.GET)
+  public static Object socketHandler(@RequestHeader(required = true) String sockRestId)
+      throws Exception {
+    System.out.println("socketHandlerGet");
+    PipedInputStream in = inputStreamList.get(sockRestId);
+    int byteRead = in.available();
+    byte[] resultBuff = new byte[byteRead];
+    try {
+      in.read(resultBuff);
+    } catch (Exception e) {
+      try {
+        in.close();
+      } catch (Exception e2) {
+      }
+      inputStreamList.remove(sockRestId);
+      throw new Exception("socket closed");
+    }
+    System.out.println("socketHandlerGetFinish");
+    return resultBuff;
+  }
 
-	@RequestMapping(value = "/socketControl/create", method = RequestMethod.POST)
-	public static String mirrorControlCreate() {
-		String tmpsessionId = getSaltString();
-		while (inputStreamList.containsKey(tmpsessionId) || outputStreamList.containsKey(tmpsessionId)) {
-			tmpsessionId = getSaltString();
-		}
-		PipedInputStream in = new PipedInputStream();
-		PipedOutputStream out = new PipedOutputStream();
-		inputStreamList.put(tmpsessionId, in);
-		outputStreamList.put(tmpsessionId, out);
-		return tmpsessionId;
-	}
+  @RequestMapping(value = "/socketControl/create", method = RequestMethod.POST)
+  public static String mirrorControlCreate() {
+    String tmpsessionId = getSaltString();
+    while (inputStreamList.containsKey(tmpsessionId)
+        || outputStreamList.containsKey(tmpsessionId)) {
+      tmpsessionId = getSaltString();
+    }
+    PipedInputStream in = new PipedInputStream();
+    PipedOutputStream out = new PipedOutputStream();
+    inputStreamList.put(tmpsessionId, in);
+    outputStreamList.put(tmpsessionId, out);
+    return tmpsessionId;
+  }
 
-	@RequestMapping(value = "/socketControl/connectTwoWay", method = RequestMethod.POST)
-	public static void mirrorControlConnectTwoWay(@RequestHeader String in, @RequestHeader String out)
-			throws Exception {
-		inputStreamList.get(in).connect(outputStreamList.get(out));
-		outputStreamList.get(in).connect(inputStreamList.get(out));
-	}
+  @RequestMapping(value = "/socketControl/connectTwoWay", method = RequestMethod.POST)
+  public static void mirrorControlConnectTwoWay(@RequestHeader String in, @RequestHeader String out)
+      throws Exception {
+    inputStreamList.get(in).connect(outputStreamList.get(out));
+    outputStreamList.get(in).connect(inputStreamList.get(out));
+  }
 
-	@RequestMapping(value = "/socketControl/connectByJson", method = RequestMethod.POST)
-	public static void mirrorControlConnectByJson(@RequestBody JsonNode config) throws Exception {
-		if (config.isArray()) {
-			for (JsonNode node : (ArrayNode) config) {
-				processConnect(node);
-			}
-		}
-		if (config.isObject()) {
-			processConnect(config);
-		}
-	}
+  @RequestMapping(value = "/socketControl/connectByJson", method = RequestMethod.POST)
+  public static void mirrorControlConnectByJson(@RequestBody JsonNode config) throws Exception {
+    if (config.isArray()) {
+      for (JsonNode node : (ArrayNode) config) {
+        processConnect(node);
+      }
+    }
+    if (config.isObject()) {
+      processConnect(config);
+    }
+  }
 
-	private final static String type = "type";
-	private final static String oneWay = "oneWay";
-	private final static String twoWay = "twoWay";
-	private final static String in = "in";
-	private final static String out = "out";
+  private final static String type = "type";
+  private final static String oneWay = "oneWay";
+  private final static String twoWay = "twoWay";
+  private final static String in = "in";
+  private final static String out = "out";
 
-	private static void processConnect(JsonNode config) throws Exception {
-		if (!config.has(type) || !config.has(in) || !config.has(out)) {
-			return;
-		}
-		switch (config.get(type).asText()) {
-		case oneWay:
-			inputStreamList.get(config.get(in).asText()).connect(outputStreamList.get(config.get(out).asText()));
-			break;
-		case twoWay:
-			inputStreamList.get(config.get(in).asText()).connect(outputStreamList.get(config.get(out).asText()));
-			outputStreamList.get(config.get(in).asText()).connect(inputStreamList.get(config.get(out).asText()));
-			break;
-		}
-	}
+  private static void processConnect(JsonNode config) throws Exception {
+    if (!config.has(type) || !config.has(in) || !config.has(out)) {
+      return;
+    }
+    switch (config.get(type).asText()) {
+      case oneWay:
+        inputStreamList.get(config.get(in).asText())
+            .connect(outputStreamList.get(config.get(out).asText()));
+        break;
+      case twoWay:
+        inputStreamList.get(config.get(in).asText())
+            .connect(outputStreamList.get(config.get(out).asText()));
+        outputStreamList.get(config.get(in).asText())
+            .connect(inputStreamList.get(config.get(out).asText()));
+        break;
+    }
+  }
 
-	@RequestMapping(value = "/socketControl/disconnect", method = RequestMethod.POST)
-	public static void mirrorControlDisconnect(@RequestHeader String sessionId) {
-		try {
-			outputStreamList.get(sessionId).close();
-		} catch (Exception e) {
-		}
-		outputStreamList.remove(sessionId);
-		try {
-			inputStreamList.get(sessionId).close();
-		} catch (Exception e) {
-		}
-		inputStreamList.remove(sessionId);
-	}
+  @RequestMapping(value = "/socketControl/disconnect", method = RequestMethod.POST)
+  public static void mirrorControlDisconnect(@RequestHeader String sessionId) {
+    try {
+      outputStreamList.get(sessionId).close();
+    } catch (Exception e) {
+    }
+    outputStreamList.remove(sessionId);
+    try {
+      inputStreamList.get(sessionId).close();
+    } catch (Exception e) {
+    }
+    inputStreamList.remove(sessionId);
+  }
 
-	@RequestMapping(value = "/socketList", method = RequestMethod.GET)
-	public static Object socketList() {
-		Map<String, Object> result = new HashMap<String, Object>();
-		result.put("inputStreamList", inputStreamList.keySet());
-		result.put("outputStreamList", outputStreamList.keySet());
-		return result;
-	}
+  @RequestMapping(value = "/socketList", method = RequestMethod.GET)
+  public static Object socketList() {
+    Map<String, Object> result = new HashMap<String, Object>();
+    result.put("inputStreamList", inputStreamList.keySet());
+    result.put("outputStreamList", outputStreamList.keySet());
+    return result;
+  }
 
-	@RequestMapping(value = "/socketClear", method = RequestMethod.GET)
-	public static void socketClear() {
-		for (String id : inputStreamList.keySet()) {
-			try {
-				inputStreamList.get(id).close();
-			} catch (Exception e) {
-			}
-			inputStreamList.remove(id);
-		}
-		for (String id : outputStreamList.keySet()) {
-			try {
-				outputStreamList.get(id).close();
-			} catch (Exception e) {
-			}
-			outputStreamList.remove(id);
-		}
-	}
+  @RequestMapping(value = "/socketClear", method = RequestMethod.GET)
+  public static void socketClear() {
+    for (String id : inputStreamList.keySet()) {
+      try {
+        inputStreamList.get(id).close();
+      } catch (Exception e) {
+      }
+      inputStreamList.remove(id);
+    }
+    for (String id : outputStreamList.keySet()) {
+      try {
+        outputStreamList.get(id).close();
+      } catch (Exception e) {
+      }
+      outputStreamList.remove(id);
+    }
+  }
 
-	public static String getSaltString() {
-		String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-		StringBuilder salt = new StringBuilder();
-		Random rnd = new Random();
-		while (salt.length() < 18) { // length of the random string.
-			int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-			salt.append(SALTCHARS.charAt(index));
-		}
-		String saltStr = salt.toString();
-		return saltStr;
-	}
+  public static String getSaltString() {
+    String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    StringBuilder salt = new StringBuilder();
+    Random rnd = new Random();
+    while (salt.length() < 18) { // length of the random string.
+      int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+      salt.append(SALTCHARS.charAt(index));
+    }
+    String saltStr = salt.toString();
+    return saltStr;
+  }
 }
